6b819e25918bf64d5256e2fcf3c932d5dcfb3bd1
kimonides <kimonide@gmail.com>
2021-03-15 22:54:23 +0200

    latest

--- stop_servers.sh
+++
@@ -1,6 +0,0 @@
-killall -9 python3 > /dev/null
-ssh node1 "killall -s SIGINT python3 > /dev/null"
-ssh node2 "killall -s SIGINT python3 > /dev/null"
-ssh node3 "killall -s SIGINT python3 > /dev/null"
-ssh node4 "killall -s SIGINT python3 > /dev/null" 
-rm ./logs/*
