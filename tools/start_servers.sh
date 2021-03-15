3c69abbf1c4db710e19d9a56e6ba9a37ceb86596
kimonides <kimonide@gmail.com>
2021-03-15 22:51:21 +0200

    latest

---
+++ tools/start_servers.sh
@@ -0,0 +1,16 @@
+# !/bin/bash
+cd ..
+python3 -u startNode.py 42069 > ./logs/"$(date +node0-42069_%F_%H-%M-%S.txt)" &
+python3 -u startNode.py 50100 > ./logs/"$(date +node0-50100_%F_%H-%M-%S.txt)" &
+
+ssh -t -t node1 "(cd distributed && python3 startNode.py 42069)" > ./logs/"$(date +node1-42069_%F_%H-%M-%S.txt)" 2>&1 &
+ssh -t -t node1 "(cd distributed && python3 startNode.py 50100)" > ./logs/"$(date +node1-50100_%F_%H-%M-%S.txt)" 2>&1 &
+
+ssh -t -t node2 "(cd distributed && python3 startNode.py 42069)" > ./logs/"$(date +node2-42069_%F_%H-%M-%S.txt)" 2>&1 &
+ssh -t -t node2 "(cd distributed && python3 startNode.py 50100)" > ./logs/"$(date +node2-50100_%F_%H-%M-%S.txt)" 2>&1 &
+
+ssh -t -t node3 "(cd distributed && python3 startNode.py 42069)" > ./logs/"$(date +node3-42069_%F_%H-%M-%S.txt)" 2>&1 &
+ssh -t -t node3 "(cd distributed && python3 startNode.py 50100)" > ./logs/"$(date +node3-50100_%F_%H-%M-%S.txt)" 2>&1 &
+ 
+ssh -t -t node4 "(cd distributed && python3 startNode.py 42069)" > ./logs/"$(date +node4-42069_%F_%H-%M-%S.txt)" 2>&1 &
+ssh -t -t node4 "(cd distributed && python3 startNode.py 50100)" > ./logs/"$(date +node4-50100_%F_%H-%M-%S.txt)" 2>&1 &
---
+++ tools/stop_servers.sh
@@ -0,0 +1,6 @@
+killall -9 python3 > /dev/null
+ssh node1 "killall -s SIGINT python3 > /dev/null"
+ssh node2 "killall -s SIGINT python3 > /dev/null"
+ssh node3 "killall -s SIGINT python3 > /dev/null"
+ssh node4 "killall -s SIGINT python3 > /dev/null" 
+rm ../logs/*
